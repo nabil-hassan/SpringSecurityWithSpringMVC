522620ae046d62d1a87a7dd883084f6c1d36e35b
Nabil Hassan <nabil.hassan@seven-telematics.com>
2017-02-13 10:14:09 +0000

    Remove shiro references to provide basic starting point for new project.

--- src/main/java/net/example/service/UserService.java
+++ src/main/java/net/example/service/UserService.java
@@ -1,23 +1,8 @@
 package net.example.service;
 
 import net.example.dao.UserDAO;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.CredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.crypto.RandomNumberGenerator;
-import org.apache.shiro.crypto.SecureRandomNumberGenerator;
-import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.SimpleByteSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.security.auth.login.AccountLockedException;
-import javax.security.auth.login.AccountNotFoundException;
-import java.math.BigInteger;
-import java.security.SecureRandom;
 
 public class UserService {
     
@@ -32,50 +17,21 @@ public class UserService {
      * Confirms whether the user is authenticated.
      */
     public boolean userIsAuthenticated() {
-        return SecurityUtils.getSubject().isAuthenticated();
+        return false;
     }
 
     /**
      * Attempts to login user; throws an Exception if unable to.
      */
     public void loginUser(String username, String password, Boolean rememberUser) throws Exception {
-        LOG.debug("Attempting to login user: {}", username);
 
-        if (username == null || password == null || username.length() == 0 || password.length() == 0) {
-            throw new IllegalArgumentException("Username and password must be specified");
-        }
-
-        Subject user = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password, Boolean.TRUE.equals(rememberUser));
-
-        try {
-            user.login(token);
-            user.getSession().setAttribute("username", username);
-            user.getSession().setAttribute("loginTime", user.getSession().getStartTimestamp());
-        } catch (CredentialsException|UnknownAccountException ex) {
-            throw new Exception("Username/password not recognised");
-        } catch (LockedAccountException ex) {
-            throw new Exception("Account is locked");
-        }
     }
 
     /**
      * Creates a new user account with the specified credentials.
      */
     public void createUser(String username, String password) throws Exception {
-        if (username == null || password == null) {
-            throw new IllegalArgumentException("Username and password must be specified");
-        }
 
-        if (userDAO.userExists(username)) {
-            throw new Exception("User: " + username +  "already exists");
-        }
-
-        String salt = new BigInteger(250, new SecureRandom()).toString(32);
-
-        Sha256Hash hashedPassword = new Sha256Hash(password, (new SimpleByteSource(salt)).getBytes());
-
-        userDAO.createUser(username, hashedPassword.toHex(), salt);
     }
 
 }
